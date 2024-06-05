127890ed1a5f74dcd3cddeb5467306d9cff94853
djairoh <ljxjah@mykolab.com>
2024-06-05 20:18:51 +0200

    update

--- src/jgf.js
+++ src/jgf.js
@@ -1,31 +1,77 @@
 function traverse (node, parentId, accu) {
   const id = accu.nextId
   accu.nextId += 1
-  if (node.nodes) {
-    // `node` is a container.
-    const metadata = Object.assign({}, node)
-    delete metadata.nodes
-    accu.nodes.push({
-      id,
-      label: node.layout,
-      metadata
-    })
-    if (parentId !== null) {
-      accu.edges.push({source: parentId, target: id})
-    }
-    node.nodes.forEach(function (child) {
-      traverse(child, id, accu)
-    })
-  } else {
-    // `node` is a window.
-    accu.nodes.push({
-      id,
-      label: node.name,
-      metadata: node
-    })
+  label = ""
+  switch (node.type) {
+    case "output":
+      label = node.name
+      break;
+    case "workspace":
+      label = node.name
+      break;
+    case "dockarea":
+      label = node.layout
+      break;
+    case "con":
+      if (node.nodes.length != 0) {
+        // split layout
+        label = node.layout
+      } else {
+        // window
+        accu.nodes.push({
+          id,
+          label: node.name + "\n" + node.window_properties.class,
+          metadata: node
+        })
+        accu.edges.push({source: parentId, target: id})
+        return;
+      }
+      break;
+  }
+
+  const metadata = Object.assign({}, node)
+  delete metadata.nodes
+  accu.nodes.push({
+    id,
+    label: label,
+    metadata
+  })
+  if (parentId !== null) {
     accu.edges.push({source: parentId, target: id})
   }
+  node.nodes.forEach(function (child) {
+    traverse(child, id, accu)
+  })
 }
+
+// function _traverse (node, parentId, accu) {
+//   const id = accu.nextId
+//   accu.nextId += 1
+//   if (node.nodes) {
+//     // `node` is a container.
+//     const metadata = Object.assign({}, node)
+//     delete metadata.nodes
+//     accu.nodes.push({
+//       id,
+//       label: node.layout,
+//       metadata
+//     })
+//     if (parentId !== null) {
+//       accu.edges.push({source: parentId, target: id})
+//     }
+//     node.nodes.forEach(function (child) {
+//       traverse(child, id, accu)
+//     })
+//   } else {
+//     // `node` is a window.
+//     accu.nodes.push({
+//       id,
+//       label: node.name,
+//       metadata: node
+//     })
+//     accu.edges.push({source: parentId, target: id})
+//   }
+// }
 
 function toJgf (node) {
   const nodes = []
